36d82e99447be91904347511c2bea741484e7ea3
JerryDeveloper01 <annexnoobdevloper@gmail.com>
2025-12-25 18:57:35 +0530

    Update environment variable defaults in config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "26713214"))
-API_HASH = getenv("API_HASH", "fc87c0fb26949deb0bc940dd35c1658d")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7752941299").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "-1002819546741")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002647218657"))
+LOG_GROUP = getenv("LOG_GROUP", "")
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "link4url.com")
